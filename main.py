8ed29f065323322725c3c08b864842d59cb616bd
Stig <stig@stig.io>
2018-07-26 21:56:38 +0200

    blah

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import sys
 import network
 import time
 import machine
-import neopixel
-import upip
-import ujson
 import ubinascii
-from umqtt.robust import MQTTClient
-import sys
+import ujson
+
+import neopixel # for blinky flashy thingy
+import CCS811 # for gas sensor
+
+from umqtt.simple import MQTTClient
+
 
 with open('farnsworth.json') as fp:
     config = ujson.loads(fp.read())
@@ -28,8 +31,12 @@ if ap_if.active():
 np = neopixel.NeoPixel(machine.Pin(0), 16)
 
 default = []
-for i in range(8): default.append((255,0,255))
-for i in range(8): default.append((0,255,0))
+
+#ecc486 skin
+#135,206,250
+
+for i in range(8): default.append((28, 251, 255))
+for i in range(8): default.append((255, 232, 150))
 
 
 def standard(np):
@@ -81,22 +88,35 @@ def on_receive(t, m):
     blink()
     flash(times=1)
 
-c = MQTTClient(client_id = CLIENT_ID,
-               server     = config['mqtt']['server'],
-               user       = config['mqtt']['user'],
-               password   = config['mqtt']['password'],
-               port       = config['mqtt']['port'],
-               ssl        = config['mqtt']['ssl']
-)
 
-c.set_callback(on_receive)
 
-if not c.connect(clean_session = False):
-    print("Session being set up")
-    c.subscribe(config['mqtt']['topic'])
+i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4))
+s = CCS811.CCS811(i2c, addr=91)
+
 
 while True:
-    c.wait_msg()
+    try:
 
-c.disconnect()
+        c = MQTTClient(client_id = CLIENT_ID,
+                       server     = config['mqtt']['server'],
+                       user       = config['mqtt']['user'],
+                       password   = config['mqtt']['password'],
+                       port       = config['mqtt']['port'],
+                       ssl        = config['mqtt']['ssl']
+        )
+        c.set_callback(on_receive)
+        c.connect()
+        c.subscribe(config['mqtt']['topic'])
+        while True:
+            c.wait_msg()
+            time.sleep_ms(250)
+            if s.data_ready():
+                print('eCO2: %d ppm, TVOC: %d ppb' % (s.eCO2, s.tVOC))
+                # p.publish("hackeriet/environment/eCO2", str(s.eCO2), True)
+                # p.publish("hackeriet/environment/tVOC", str(s.tVOC), True)
+    except OSError as e:
+        print("Woops, trying to do stuff again\n")
+
+
+
 
